14c85afac32b344d711fe7f532ad5dd4229297a0
shamili-surya <119722978+shamili-surya@users.noreply.github.com>
2023-08-18 12:04:21 +0530

    import java.util.Scanner;  public class MultiplesOfThree {          public static void main(String[]args) {         Scanner scanner=new Scanner(System.in);         int n=scanner.nextInt();         for(int i=1;i<=n;i++){             System.out.printf("%d ",3*i);         }        } }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ public class MultiplesOfThree {
+         public static void main(String[]args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.printf("%d ",3*i);
+        }
+       }
+}
